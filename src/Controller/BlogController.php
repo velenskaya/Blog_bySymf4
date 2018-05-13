a1891c4aec02c25de31c63ae09dc91b2aeaf9cbb
Oksana <fsoksanna@gmail.com>
2018-05-13 15:05:27 +0300

    no commits

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class TestController extends Controller
+class BlogController extends Controller
 {
     /**
-     * @Route("/test", name="test")
+     * @Route("/blog", name="blog")
      */
     public function index()
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
         ]);
     }
 }
